a41ff141a596f16605f35a55c265f418b64146eb
maggievu <mvu@pdftron.com>
2022-05-06 16:26:03 -0700

    create different client URLs

--- server/index.js
+++ server/index.js
@@ -9,7 +9,11 @@ const NODE_PORT = process.env.REACT_APP_NODE_PORT || 3100;
 const OPTIONS = {
   SERVER_ROOT: `http://localhost`,
   PORT: NODE_PORT,
-  CORS_OPTIONS: { origin: `http://localhost:${PORT}`, credentials: true },
+  CORS_OPTIONS: { origin: [
+    `http://localhost:${PORT}`,
+    `http://first.mydomain.com:3000`,
+    `http://second.mydomain.com:3001`,
+  ], credentials: true },
   // COOKIE_SETTING: { sameSite: 'none', secure: true },
 };
 
